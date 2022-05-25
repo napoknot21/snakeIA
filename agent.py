f819349d70630b447e7432cad00d92d10eb027fe
napoknot21 <kinbrian5@hotmail.com>
2022-05-25 23:49:35 +0200

    headers and functions for agent.py

--- agent.py
+++ agent.py
@@ -1,1 +1,44 @@
-import pygame
+import torch
+import random
+import numpy as np
+
+from collections import deque
+from game import SnakeGameAI, Direction, Point
+
+#We define some const
+MAX_MEMORY = 100_100
+BATCH_SIZE = 1000
+LR = 0.001
+
+class Agent :
+
+    def __init__ (self) :
+        pass
+
+    
+    def get_state (self, game) :
+        pass
+
+    
+    def remember (self, state, action, reward, next_state, done) :
+        pass
+
+
+    def train_long_memory (self) :
+        pass
+
+
+    def train_short_memory (self) :
+        pass
+
+
+    def get_action (self, state) :
+        pass
+
+
+def train () :
+    pass
+
+
+if __name__ == '__main__' :
+    train ()

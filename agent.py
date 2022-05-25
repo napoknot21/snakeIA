64f7209dbec16327a1764c2df6ffab2d340b4a7c
napoknot21 <kinbrian5@hotmail.com>
2022-05-26 01:17:01 +0200

    get_state() implementation

--- agent.py
+++ agent.py
@@ -21,8 +21,54 @@ class Agent :
 
     
     def get_state (self, game) :
-        
+        head = game.snake[0] #snake head (a list)
 
+        #Points next to the head in all directions
+        point_l = Point(head.x - 20, head.y) # 20 beacuase we have defined bloc_size = 20
+        point_r = Point(head.x + 20, head.y)
+        point_u = Point(head.x, head.y - 20)
+        point_d = Point(head.x, head.y + 20)
+
+        #current directions
+        dir_l = game.direction == Direction.LEFT
+        dir_r = game.direction == Direction.RIGHT
+        dir_u = game.direction == Direction.UP
+        dir_d = game.direction == Direction.DOWN
+
+        state = [
+            #Danger straight
+            (dir_r and game.is_collision(point_r)) or
+            (dir_l and game.is_collision(point_l)) or 
+            (dir_u and game.is_collision(point_u)) or
+            (dir_d and game.is_collision(point_d)),
+
+            #Danger right
+            (dir_u and game.is_collision(point_r)) or
+            (dir_d and game.is_collision(point_l)) or
+            (dir_l and game.is_collision(point_u)) or
+            (dir_r and game.is_collision(point_d)),  
+
+            #Danger left
+            (dir_d and game.is_collision(point_r)) or
+            (dir_u and game.is_collision(point_l)) or
+            (dir_r and game.is_collision(point_u)) or
+            (dir_l and game.is_collision(point_d),
+
+            #move direction
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
+
+            #food location
+            game.food.x < game.head.x, #food in the head snake left 
+            game.food.x > game.head.x, #right
+            game.food.y < game.head.y, #food up
+            game.food.y > game.head.y #food down
+        ]
+        
+        return np.array(state, dtype=int)
+        
     
     def remember (self, state, action, reward, next_state, done) :
         pass

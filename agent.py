f45a8176987b4bf6620f6255a1fb47776c8b422a
napoknot21 <kinbrian5@hotmail.com>
2022-05-26 02:03:15 +0200

    train short and long memory implementation

--- agent.py
+++ agent.py
@@ -17,6 +17,8 @@ class Agent :
         self.epsilon = 0 #randomness
         self.gama = 0 #discount rate
         self.memory = deque(max_len=MAX_MEMORY) #popleft()
+        self.mode = None
+        self.traine = None
         # TODO model, trainer
 
     
@@ -70,20 +72,39 @@ class Agent :
         return np.array(state, dtype=int)
         
     
-    def remember (self, state, action, reward, next_state, done) :
-        pass
+    def remember (self, state, action, reward, next_state, done) : 
+        self.memory.append((state, action, reward, next_state, done)) # popleft if MAX_MEMORY is full
 
 
     def train_long_memory (self) :
-        pass
+        if len(self.memory) > BATCH_SIZE :
+            mini_sample = random.sample(self.memory, BATCH_SIZE) #list of tuples
+        else :
+            mini_simple = self.memory
 
+        states, actions, rewards, next_states, dones = zip (*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
+        
 
     def train_short_memory (self, state, action, reward, next_state, done) :
-        pass
+        self.trainer.train_step(state, action, reward, next_state, done)
 
 
     def get_action (self, state) :
-        pass
+        #random moves : tradeoff exploration (exploitation)
+        self.epsilon = 80 - self.n_games #number of games aprox (hardcoded)
+        final_move = [0, 0, 0]
+        if random.randint (0, 200) < self.epsilon :
+            move = random.randint(0, 2)
+            final_move[move] = 1
+        else :
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model.predict(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+            
+        return final_move
+
 
 
 def train () :
